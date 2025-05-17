cd8150cad742d625e35a85f546371557287ea529
eight <18252110983@163.com>
2025-05-17 21:37:34 +0800

    first commit

---
+++ agent/sub_agents/sensitive_word/SensitiveWord.py
@@ -0,0 +1,39 @@
+import os
+from google.adk.agents import Agent
+from google.adk.tools.agent_tool import AgentTool
+from google.genai.types import GenerateContentConfig
+from google.adk.agents import LlmAgent
+from google.adk.models.lite_llm import LiteLlm
+# from travel_concierge.sub_agents.booking import prompt
+
+
+_promopt = """
+ 你是一个负责审核文案是否包含敏感词的agent，请检查输入的文案是否包含敏感词，
+ 如果包含敏感词，请返回敏感词，并解释，否则返回"no sensitive word"。
+    请关注如下但不仅限于以下的敏感词：
+    1. 政治敏感词
+    2. 宗教敏感词
+    3. 暴力敏感词
+    4. 暴力恐吓敏感词
+    5. 色情敏感词
+    6. 舆论对立敏感词
+ 如果输入的文案中不包含敏感词，那么请返回"no sensitive word"。
+"""
+qwen_model = LiteLlm(
+    model="openai/Qwen/Qwen3-235B-A22B",
+    api_base=os.environ.get("OPENAI_API_BASE"),
+    api_key=os.environ.get("OPENAI_API_KEY"),
+    extra_body= {
+        "enable_thinking":False,
+        "stream":False
+    },
+    stream=False,  
+)
+
+
+sensitive_word = Agent(
+    model="gemini-2.0-flash-001",
+    name="sensitive_word",
+    description="""check whether the input contains sensitive words""",
+    instruction=_promopt
+)
